fe815d27e4bd27b2dd8990efac9855cbf625d8f4
Dennif <dennif.pa@gmail.com>
2025-03-27 11:09:24 -0700

    student.py started

--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,10 @@
-# add your Student class here!
+class Student:
+    def __init__(self, name, year, classes):
+        self.name = name 
+        self.year = year
+        self.classes = classes
+        
+    def add_class(self,new_class):
+        self.classes.append(new_class)
+        
+    
