55bbbbd7665cae01de5b653b6eec8f19266c8023
Dennif <dennif.pa@gmail.com>
2025-03-31 02:47:02 -0700

    comparison added

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,1 +1,8 @@
 # add your get_student_with_more_classes function here!
+
+def get_student_with_more_classes(student1, student2):
+    
+    if len(student1.classes) > len(student2.classes):
+        return student1
+    else:
+        return student2
